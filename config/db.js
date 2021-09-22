c365b7de810700e97ac71ab0b64199876e22d4a1
brendorutherles <87491315+brendorutherles@users.noreply.github.com>
2021-09-21 22:36:12 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,5 +2,5 @@
 // To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
 
 module.exports = {
-  db: process.env.DB
+  db: 'mongodb+srv://brendo:brendo1996@cluster0.alkvg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 };
